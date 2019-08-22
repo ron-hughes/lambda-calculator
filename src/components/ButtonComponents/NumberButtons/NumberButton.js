b3f706b2f57b78909dc81a230fef39af50fdc77c
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-08-22 00:23:44 -0400

    added all React buttons to DOM using props

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+//  console.log(props)
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+  <div><button>{props.num}</button></div>
+    ) 
+      // {/* Display a button element rendering the data being passed down from the parent container on props */}
 };
+
+export default NumberButton
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { numbers } from './../../../data.js';
+import NumberButton from './NumberButton.js';
 
 //import any components needed
 
@@ -6,11 +8,23 @@ import React from "react";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+
+  const [num] = useState(numbers)
+  // console.log(num)
   return (
     <div>
+      
+  
+      {num.map(e => {
+        return <NumberButton key={e} num={e} />
+      })}
+      
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component */}
     </div>
   );
+
 };
+
+export default Numbers
